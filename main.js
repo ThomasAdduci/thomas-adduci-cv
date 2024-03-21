3bae47c6e592fe2dff47c0117bdfcb54201a7d58
Thomas Adduci <99020657+ThomasAdduci@users.noreply.github.com>
2024-03-20 23:07:45 -0300

    Added the certifications section

--- main.js
+++ main.js
@@ -5,5 +5,15 @@ window.addEventListener('scroll', function() {
     } else {
       navbar.classList.remove('scrolling');
     }
+});
+
+const navLinks = document.querySelectorAll('#navbar a');
+navLinks.forEach(link => {
+  link.addEventListener('click', event => {
+    event.preventDefault();
+    const targetId = link.getAttribute('href').substring(1);
+    const targetSection = document.getElementById(targetId);
+    targetSection.scrollIntoView({ behavior: 'smooth' });
   });
+});
   
