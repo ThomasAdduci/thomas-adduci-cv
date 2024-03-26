84015a2f199dd9ca50d94b80d73054cacdae9963
Thomas Adduci <99020657+ThomasAdduci@users.noreply.github.com>
2024-03-26 01:47:52 -0300

    Fixed scroll problem with the navbar

--- main.js
+++ main.js
@@ -7,12 +7,18 @@ window.addEventListener('scroll', function() {
     }
 });
 
+const navbarHeight = document.getElementById('navbar').offsetHeight;
+const additionalMargin = 20;
 const navLinks = document.querySelectorAll('#navbar a');
 navLinks.forEach(link => {
   link.addEventListener('click', event => {
     event.preventDefault();
     const targetId = link.getAttribute('href').substring(1);
     const targetSection = document.getElementById(targetId);
-    targetSection.scrollIntoView({ behavior: 'smooth' });
+    const targetOffset = targetSection.offsetTop - navbarHeight - additionalMargin;
+    window.scrollTo({
+      top: targetOffset,
+      behavior: 'smooth'
+    });
   });
 });
